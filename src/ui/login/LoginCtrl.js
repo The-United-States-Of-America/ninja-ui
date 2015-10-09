882aa6e79a03cbf99bad00d62e525475c8abfb02
Erica Braunschweig <braune@rpi.edu>
2015-10-09 17:28:18 -0400

    working login

---
+++ src/ui/login/LoginCtrl.js
@@ -0,0 +1,6 @@
+export default class LoginCtrl {
+    constructor($scope) {
+        this.$scope = $scope;
+    }
+
+}
--- src/ui/login/index.js
+++ src/ui/login/index.js
@@ -6,11 +6,21 @@ import uiRouter from 'angular-ui-router';
 import ngAnimate from 'angular-animate';
 import ngMaterial from 'angular-material';
 import LoginTemplate from './login.html';
+import LoginCtrl from './LoginCtrl';
 
-angular.module("LoginApp", [
+angular.module(namespace, [
     uiRouter,
     ngAnimate,
     ngMaterial
-]);
+])
+
+.config(function($stateProvider) {
+    $stateProvider.state("login", {
+        url: "/login",
+        template: LoginTemplate,
+        controller: LoginCtrl
+    });
+
+});
 
 
