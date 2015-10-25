76d89245706f1136e99d2e12bc26946b81e9264b
Erica Braunschweig <braune@rpi.edu>
2015-10-25 15:25:08 -0400

    Login Update
    Fixed Coffeescript to AngularJS conversion errors

--- src/ui/login/LoginCtrl.js
+++ src/ui/login/LoginCtrl.js
@@ -1,5 +1,23 @@
 export default class LoginCtrl {
   constructor($scope) {
       this.$scope = $scope;
+      this.$state = $state;
+
+      this.$scope.user = {
+        error: false,
+        error_message: ''
+      };
+
+      this.$scope.login = this.login.bind(this);
   }
+
+  login() {
+    return AuthService.login($scope.user.email, $scope.user.password).then(function(user_data) {
+        //Success, user object returned. Go to patient page for now.
+        $state.go('ui.dash.patient');
+      }, function(err) {
+        $scope.user.error = true;
+        return $scope.user.error_message = err;
+      });
+  };
 }
--- src/ui/login/index.js
+++ src/ui/login/index.js
@@ -9,7 +9,6 @@ import LoginCtrl from './LoginCtrl';
 import LoginTemplate from './login.html';
 import './login.css';
 
-
 angular.module(namespace, [
     uiRouter,
     ngAnimate,
@@ -24,8 +23,37 @@ angular.module(namespace, [
     });
 })
 
-.controller(namespace, function($scope) {
-  $scope.authenticate = function() {
+.service('authService', function() {
+  return {
+    loggedIn: function() {
+      return $window.sessionStorage.user;
+    },
 
+    login: function(email, pass) {
+      var dataPromise, messageData;
+      dataPromise = $q.defer();
+      messageData = {
+        email: email,
+        password: pass
+      };
+
+      $http({
+        url: 'localhost:8001/client/login',
+        method: 'POST',
+        data: JSON.stringify(messageData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).success(function(data, status, headers, config) {
+        $window.sessionStorage.token = data.token;
+        $window.sessionStorage.user = data.user;
+        return dataPromise.resolve(data);
+      }).error(function(data, status, headers, config) {
+        $window.sessionStorage.removeItem('token');
+        $window.sessionStorage.removeItem('user');
+        return dataPromise.reject(data.m);
+      });
+      return dataPromise.promise;
+    }
   }
 });
