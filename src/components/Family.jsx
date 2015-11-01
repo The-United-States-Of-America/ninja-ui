4902ee586f0307757ea967b626a8167b24e3f449
Ryan Delaney <rrdelaney@outlook.com>
2015-11-01 18:21:24 -0500

    added famiyl view

--- src/components/Family.jsx
+++ src/components/Family.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Gravatar from 'react-gravatar'
+import { family, member } from './Family.css'
 
 export default function Family () {
-  return <div>
-    <h3>You don't have a family.</h3>
-    <h3>You'll be lonely forever.</h3>
+  let myFamily = [
+    {
+      firstName: 'Ryan',
+      lastName: 'Delaney',
+      email: 'delanr3@rpi.edu',
+      id: '1'
+    },
+    {
+      firstName: 'Ryan',
+      lastName: 'Delaney',
+      email: 'delanr3@rpi.edu',
+      id: '3'
+    },
+    {
+      firstName: 'Ryan',
+      lastName: 'Delaney',
+      email: 'delanr3@rpi.edu',
+      id: '4'
+    },
+    {
+      firstName: 'Ryan',
+      lastName: 'Delaney',
+      email: 'delanr3@rpi.edu',
+      id: '5'
+    },
+    {
+      firstName: 'Pranav',
+      lastName: 'Sathyanarayanan',
+      email: 'sathyp@rpi.edu',
+      id: '2'
+    }
+  ]
+
+  return <div className={family}>
+    {myFamily.map(fam => <div key={fam.id} className={member}>
+      <Gravatar className="ui small centered circular image" email={fam.email} size={500}/>
+      <h3>{fam.firstName} {fam.lastName}</h3>
+    </div>)}
   </div>
 }
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,36 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Messages extends Component {
-  constructor(props) {
-    super(props)
+export default function Messages (props) {
+  let messages = [
+    {title: 'A Message', body: 'do a thing'},
+    {title: 'A Message', body: 'do a thing'},
+    {title: 'A Message', body: 'do a thing'},
+    {title: 'A Message', body: 'do a thing'},
+    {title: 'A Message', body: 'do a thing'},
+    {title: 'A Message', body: 'do a thing'},
+    {title: 'A Message', body: 'do a thing'},
+    {title: 'A Message', body: 'do a thing'},
+    {title: 'A Message', body: 'do a thing'},
+    {title: 'B Message', body: 'y0 r0l0'}
+  ]
 
-    this.state = {}
-    this.state.messages = [
-      {title: 'A Message', body: 'do a thing'},
-      {title: 'A Message', body: 'do a thing'},
-      {title: 'A Message', body: 'do a thing'},
-      {title: 'A Message', body: 'do a thing'},
-      {title: 'A Message', body: 'do a thing'},
-      {title: 'A Message', body: 'do a thing'},
-      {title: 'A Message', body: 'do a thing'},
-      {title: 'A Message', body: 'do a thing'},
-      {title: 'A Message', body: 'do a thing'},
-      {title: 'B Message', body: 'y0 r0l0'}
-    ]
-  }
-
-  messagesList(messages) {
-    return <div className="ui celled list">
-      {messages.map((message, idx) => (
-        <div className="item" key={idx}>
-          <h3>{message.title}</h3>
-          <p>{message.body}</p>
-        </div>
-      ))}
-    </div>
-  }
-
-  render() {
-    return this.messagesList(this.state.messages)
-  }
+  return <div className="ui celled list">
+    {messages.map((message, idx) => (
+      <div className="item" key={idx}>
+        <h3>{message.title}</h3>
+        <p>{message.body}</p>
+      </div>
+    ))}
+  </div>
 }
