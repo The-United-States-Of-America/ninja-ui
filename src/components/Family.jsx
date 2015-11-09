6b20ec03b0c7a283c766979e989cfe4e9a92a523
Pranav Sathyanarayanan <sathyp@rpi.edu>
2015-11-09 17:01:20 -0500

    Invite members to family

--- src/components/Family.jsx
+++ src/components/Family.jsx
@@ -49,14 +49,44 @@ export default class Family extends Component {
     await* clients.map(client => postJson(`${DBSRV}/family/invite`, { clientId: client.id, familyId: id }))
   }
 
+  async handleFamilyInvitation () {
+    console.log(this.state.newFamilyMembers);
+    let clientResponse = await* this.state.newFamilyMembers.split(',').map(email => fetch(`${DBSRV}/client/get/${email.trim()}`))
+    let clients = await* clientResponse.map(c => c.json())
+
+    await* clients.map(client => postJson(`${DBSRV}/family/invite`, { clientId: client.id, familyId: this.props.family.id }))
+    this.setState({ newFamilyMembers: '' })
+  }
+
   render () {
     if (this.state.family) {
-      return <div className={`${family} ui basic segment`}>
-        {this.state.family.map(fam => <div key={fam.id} className={member}>
-          <Gravatar className='ui small centered circular image' email={fam.email} size={500}/>
-          <h3>{fam.firstName} {fam.lastName}</h3>
-        </div>)}
+      return <div>
+        <div className={`${family} ui basic segment`}>
+          {this.state.family.map(fam => <div key={fam.id} className={member}>
+            <Gravatar className='ui small centered circular image' email={fam.email} size={500}/>
+            <h3>{fam.firstName} {fam.lastName}</h3>
+          </div>)}
+        </div>
+        <div className='ui basic segment'>
+          <h2>Invite more members to: {this.props.family.name}</h2>
+            <div className='ui form'>
+              <div className='field'>
+                <label>Family Members (Comma Separated)</label>
+                <div className='ui fluid input'>
+                  <input
+                    type='text'
+                    placeholder='Members'
+                    value={this.state.new_members}
+                    onChange={::this.handleUpdate('newFamilyMembers')}/>
+                </div>
+              </div>
+              <button className='ui fluid blue button' type='submit' onClick={::this.handleFamilyInvitation}>
+                Submit
+              </button>
+            </div>
+        </div>
       </div>
+
     } else if (this.props.family) {
       return <div className='ui basic segment'>
         <div className='ui active loader' style={{marginTop: '3rem'}}></div>
